ee3052ff3f0e9998c211c502b741c983427efdf0
Gustavo Couto <gtlcouto@Gustavos-MacBook-Pro.local>
2015-02-23 11:34:59 -0800

    Initial Commit

---
+++ GCLibrary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GCLibrary
+//
+//  Created by Gustavo Couto on 2015-02-23.
+//  Copyright (c) 2015 Gustavo Couto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GCLibrary/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GCLibrary
+//
+//  Created by Gustavo Couto on 2015-02-23.
+//  Copyright (c) 2015 Gustavo Couto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
